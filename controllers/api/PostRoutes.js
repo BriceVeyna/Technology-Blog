13068383c068dc8cf4d11ece6b0799cdacb49b4c
Brice Veyna <bricev99@gmail.com>
2022-08-04 08:16:59 -0700

    Add routes

---
+++ controllers/api/PostRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Post, Comment } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll();
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [{ model: Comment }],
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with that id' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const postData = await Post.create(req.body);
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const Comment = require('../../models/Comment');
+
+
+
+module.exports = router;
